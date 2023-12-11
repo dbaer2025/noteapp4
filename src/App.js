5c3c567ed0193ff29b6afda89e84e9ced386077c
dbaer2025 <dbaerfla@gmail.com>
2023-12-11 17:37:33 -0500

    no

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react'
-import './App.css';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-import {NoteNavBar, NoteCollection} from "./ui-components";
-class App extends Component {
-  render() {
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
+
+function App({ signOut }) {
   return (
-    <div className="App"><header className="App-header">
-        <NoteNavBar/><NoteCollection/>
-      </header></div>
-    );
-}
+    <View className="App">
+      <Card>
+        <Image src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
 }
 
 export default withAuthenticator(App);
